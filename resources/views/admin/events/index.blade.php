c295786e8645b641dbbd2768acd856cf59db2445
bil1234n <bilalnesru123456789@gmail.com>
2025-11-25 12:05:25 +0300

    Update index.blade.php

--- resources/views/admin/events/index.blade.php
+++ resources/views/admin/events/index.blade.php
@@ -99,7 +99,7 @@
                         </div>
                         <div class="col-md-6">
                             <label class="form-label text-muted small fw-bold">Date & Time</label>
-                            <input type="datetime-local" name="event_date" class="form-control" required>
+                            <input type="datetime-local" name="event_date" class="form-control">
                         </div>
                         <div class="col-md-12">
                             <label class="form-label text-muted small fw-bold">Description (Optional)</label>
