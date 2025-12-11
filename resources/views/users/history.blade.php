096d03f181b11868ad249626414a343ecfa5612f
bil1234n <bilalnesru123456789@gmail.com>
2025-12-11 20:19:31 +0300

    Update history.blade.php

--- resources/views/users/history.blade.php
+++ resources/views/users/history.blade.php
@@ -31,7 +31,7 @@
                 </button>
             </form>
             <!-- New Message Action -->
-            <a href="{{ url('home') }}" class="inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-lg shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transition-all">
+            <a href="{{ url('/') }}" class="inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-lg shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transition-all">
                 🏠
                 Home
             </a>
